Event Center variance visualization and bug fix for no MCI cases during bootstrapping in debm

pyebm/debm.py
@@ -65,14 +65,21 @@ def fit(DataIn,MethodOptions=False,VerboseOptions=False,Factors=['Age','Sex','IC
         bs_ptid_AD_raw=resample(ptid_AD_raw,random_state=i);
         bs_data_CN_raw=resample(data_CN_raw,random_state=i);
         bs_ptid_CN_raw=resample(ptid_CN_raw,random_state=i);
-        bs_data_MCI_raw=resample(data_MCI_raw,random_state=i);
-        bs_ptid_MCI_raw=resample(ptid_MCI_raw,random_state=i);                        
-        bs_data_all=np.concatenate((bs_data_AD_raw,bs_data_CN_raw,bs_data_MCI_raw))  
-        bs_ptid_all=np.concatenate((bs_ptid_AD_raw,bs_ptid_CN_raw,bs_ptid_MCI_raw))  
+        if len(data_MCI_raw)>0:
+            bs_data_MCI_raw=resample(data_MCI_raw,random_state=i);
+            bs_ptid_MCI_raw=resample(ptid_MCI_raw,random_state=i);                        
+            bs_data_all=np.concatenate((bs_data_AD_raw,bs_data_CN_raw,bs_data_MCI_raw))  
+            bs_ptid_all=np.concatenate((bs_ptid_AD_raw,bs_ptid_CN_raw,bs_ptid_MCI_raw)) 
+        else:
+            bs_data_all=np.concatenate((bs_data_AD_raw,bs_data_CN_raw))  
+            bs_ptid_all=np.concatenate((bs_ptid_AD_raw,bs_ptid_CN_raw)) 
         labels_AD = np.zeros(len(bs_data_AD_raw))+3
         labels_CN = np.zeros(len(bs_data_CN_raw))+1
-        labels_MCI = np.zeros(len(bs_data_MCI_raw))+2
-        labels_all=np.concatenate((labels_AD,labels_CN,labels_MCI))
+        if len(data_MCI_raw)>0:
+            labels_MCI = np.zeros(len(bs_data_MCI_raw))+2
+            labels_all=np.concatenate((labels_AD,labels_CN,labels_MCI))
+        else:
+            labels_all=np.concatenate((labels_AD,labels_CN))
         if type(DVO.WriteBootstrapData)==str:
             str_out=DVO.WriteBootstrapData+'_'+str(i)+'.csv'
             Dbs=pd.DataFrame(bs_data_all[:,:,0],columns=BiomarkersList)
@@ -91,7 +98,7 @@ def fit(DataIn,MethodOptions=False,VerboseOptions=False,Factors=['Age','Sex','IC
     for i in range(len(data_AD_raw_list)): ## For each bootstrapped iteration
         ## Reject possibly wrongly labeled data 
         if len(data_AD_raw_list)>1:
-            print([i]),
+            print([i],end=""),
         data_AD_raw = data_AD_raw_list[i];
         data_CN_raw = data_CN_raw_list[i];
         Data_all =  data_all_list[i];                             
@@ -176,6 +183,7 @@ def fit(DataIn,MethodOptions=False,VerboseOptions=False,Factors=['Age','Sex','IC
     ## Visualize Results
     if DVO.Ordering==1:
         util.VisualizeOrdering(BiomarkersList, pi0_all,pi0_mean,DVO.PlotOrder);
+        util.VisualizeEventCenters(BiomarkersList,pi0_mean, pi0_all,event_centers_all);
     if DVO.Distributions==1:
         params_all=[params_opt];
         util.VisualizeBiomarkerDistribution(Data_all,params_all,BiomarkersList);

pyebm/util.py
@@ -276,12 +276,44 @@ def VisualizeOrdering(labels, pi0_all,pi0_mean, plotorder):
         datapivot = datapivot.reindex(newindex)        
     xticks = np.arange(len(labels)) + 1
     datapivot = datapivot[datapivot.columns].astype(float)
-    heatmap = sns.heatmap(datapivot, cmap = 'binary', xticklabels=xticks, vmin=0, vmax=len(pi0_all))
-    fig = heatmap.get_figure()
+    fig, ax = plt.subplots(1,1,figsize=(7, 7))
+    heatmap = sns.heatmap(datapivot, cmap = 'binary', xticklabels=xticks, vmin=0, vmax=len(pi0_all),ax=ax)
+    plt.sca(ax)
     plt.title('Positional variance diagram of the central ordering')
     plt.yticks(rotation=0) 
     plt.show()
     
+def VisualizeEventCenters(labels, pi0_mean, pi0_all, event_centers):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import copy as cp
+    
+    event_centers_ordered = cp.deepcopy(event_centers)
+    for i in range(len(event_centers)):
+        count=-1
+        for x in pi0_all[i]:
+            count=count+1;
+            event_centers_ordered[i][x] = cp.deepcopy(event_centers[i][count])
+    
+    evn = np.asarray(event_centers_ordered)
+    evn_full = np.mean(evn,axis=0)
+    
+    fig, ax = plt.subplots(1,1,figsize=(7, 7))
+    for i in range(len(labels)):
+        (_, caps, _)=ax.errorbar(evn_full[i],len(labels)-1-pi0_mean.index(i),xerr=np.std(evn[:,i],axis=0),fmt='--o',color='b',alpha=0.7,barsabove=True,capsize=1)
+        for cap in caps:
+            cap.set_markeredgewidth(5)
+    ax.set_yticks(np.arange(0,len(labels)-1+.1,1))
+    ax.set_yticklabels([labels[x] for x in pi0_mean[::-1]],rotation=45)
+    #ax.get_yaxis().set_visible(False)
+    ax.set_xlim([0,1])
+    ax.set_xticks(np.arange(0,1.,0.1))
+    ax.set_xticklabels(np.arange(0,1.,0.1))
+    ax.grid(b=True, axis='x', color='k', linestyle='--',alpha=0.5,which='major')
+    ax.set_xlabel('Disease Stage')
+    ax.set_ylim([-0.5,0.5+len(labels)-1])
+    ax.set_title('Event Center Variance Diagram')
+    plt.subplots_adjust(wspace=0, hspace=0)
     
 def VisualizeStaging(subj_stages,Diagnosis,Labels):
     import numpy as np
@@ -328,7 +360,7 @@ def VisualizeStaging(subj_stages,Diagnosis,Labels):
         
     ax.set_yticks(np.arange(0,maxfreq,0.1))
     ax.set_yticklabels(np.arange(0,maxfreq,0.1),fontsize=14)
-    ax.set_xlabel('Estimated Disease State',fontsize=16)
+    ax.set_xlabel('Estimated Disease Stage',fontsize=16)
     ax.set_ylabel('Frequency of occurrences',fontsize=16)
     ax.legend(fontsize=16)
     plt.title('Patient Staging',fontsize=16)